6338f15f5f68a938b932dedb2f547e75af869393
Joshua Suskin <jsuskin85@gmail.com>
2019-05-06 09:08:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,14 @@ def using_concat(a1,a2)
   a1.concat(a2)
 end
 
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
 def using_insert(arr, e)
   arr.insert(4,e)
 end
